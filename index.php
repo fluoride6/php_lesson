f34efc2f6d41079ecbd3213762d5bb427f219b86
Anna N <Anna@anna.com>
2021-07-29 21:16:16 +0300

    Add two index.php and updated git

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$i = 0;
+while ($i < 10){
+    echo "$i <br />";
+    $i++;
+}
+
+$summa = 0;
+$i = 10;
+$n = 40;
+while ($i <= $n){
+    $summa += $i;
+    $i += 2;
+}
+echo "$summa";
+
+echo "<br />";
+
+?>
